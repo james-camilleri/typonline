9801409f87a8a26775fda52844433415972a231d
James Camilleri <james@james.mt>
2022-03-31 19:38:04 +0100

    Update broken (generated) schemas

--- cms/schemas/schema.js
+++ cms/schemas/schema.js
@@ -2,8 +2,8 @@ import { WebImageSchema as webImage } from '@james-camilleri/sanity-web-image'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import createSchema from 'part:@sanity/base/schema-creator'
 
-import post from './cms/schemas/collections/posts.js'
-import pageSettings from './cms/schemas/pages/settings.js'
+import post from './collections/posts.js'
+import pageSettings from './pages/settings.js'
 
 export default createSchema({
   name: 'default',

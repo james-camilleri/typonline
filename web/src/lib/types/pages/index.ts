dee09ec377eb649fbdc50443578c97d899006f1d
James Camilleri <james@james.mt>
2022-05-02 19:04:34 +0100

    Add performance page type

--- web/src/lib/types/pages/index.ts
+++ web/src/lib/types/pages/index.ts
@@ -1,11 +1,5 @@
-export type { Page } from './page'
-
-export type { Sample } from './sample'
-export type { Contact } from './contact'
-
 export const PAGES = {
-  SAMPLE: 'pageSample',
-  CONTACT: 'pageContact',
+  PERFORMANCE: 'performance',
 } as const
 
 export type PageId = typeof PAGES[keyof typeof PAGES]
---
+++ web/src/lib/types/pages/performance.ts
@@ -0,0 +1,5 @@
+export interface Performance {
+  _type: 'performance'
+  phrases: string[]
+  questions: string[]
+}

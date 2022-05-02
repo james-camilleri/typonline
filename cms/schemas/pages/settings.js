a8433ea3460a89b6dc63068ec6436e4f2c932e9b
James Camilleri <james@james.mt>
2022-05-02 15:24:46 +0100

    Add additional typewriter-specific settings

--- cms/schemas/pages/settings.js
+++ cms/schemas/pages/settings.js
@@ -3,12 +3,31 @@ export default {
   type: 'document',
   title: 'Settings',
   __experimental_actions: ['update', 'publish'],
+  fieldsets: [{ name: 'typewriter', title: 'Typewriter' }],
   fields: [
     {
       name: 'ngrokUrl',
       title: 'ngrok URL',
       type: 'url',
       readOnly: true,
+    },
+    {
+      name: 'charsPerLine',
+      title: 'Characters per line',
+      description:
+        'The maximum number of characters to fit in each line of text.',
+      type: 'number',
+      fieldset: 'typewriter',
+      validation: (Rule) => Rule.required().min(10),
+    },
+    {
+      name: 'newlineRotationDegrees',
+      title: 'Carriage motor rotation (degrees)',
+      description:
+        'Degrees of rotation required for a full pull of the carriage. (This only needs to be modified if the typewriter is changed.)',
+      type: 'number',
+      fieldset: 'typewriter',
+      validation: (Rule) => Rule.required(),
     },
   ],
 }

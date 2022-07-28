f69d930ff1709004848c70ef96148907364add35
James Camilleri <james@james.mt>
2022-07-28 22:31:39 +0100

    Add public page placeolders

---
+++ web/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+  export const prerender = true
+</script>
+
+<script lang="ts">
+  import Title from '$lib/components/global/Title.svelte'
+  import Grid from '$lib/components/layout/Grid.svelte'
+  import Transition from '$lib/components/transition/Transition.svelte'
+
+  import CONFIG from '$lib/config'
+</script>
+
+<Title text={CONFIG.GENERAL.siteTitle} />
+<Grid>
+  <p>(coming soon)</p>
+  <Transition order={0} />
+</Grid>

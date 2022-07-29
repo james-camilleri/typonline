f11d36aa2130030f0dacb7c59736dbb8cf843ea6
James Camilleri <james@james.mt>
2022-07-29 12:39:09 +0100

    Add typing effect to title

--- web/src/lib/components/global/Title.svelte
+++ web/src/lib/components/global/Title.svelte
@@ -6,20 +6,35 @@
   export let hidden: boolean = false
 
   const prefix = CONFIG.GENERAL.siteTitle
+
+  function random(min, max) {
+    return Math.random() * (max - min) + min
+  }
+
+  let displayTitle = ''
+  text.split('').map((letter, i) =>
+    setTimeout(() => {
+      displayTitle += letter
+    }, 100 * i * random(0.8, 1.2)),
+  )
 </script>
 
 <svelte:head>
   <title>{prefix ? `${prefix} – ` : ''}{text}</title>
 </svelte:head>
 
-<h1 class:screen-reader-only={hidden}>{text}</h1>
+<h1 class:screen-reader-only={hidden}>{displayTitle}</h1>
 {#if subtitle}
   <h2 class:screen-reader-only={hidden}>{subtitle}</h2>
 {/if}
 
 <style lang="scss">
-  // Remove default (large) spacing before subtitle.
+  h1 {
+    margin-bottom: var(--xl);
+  }
+
   h2 {
+    // Remove default (large) spacing before subtitle.
     margin-top: 0;
     font-size: var(--lg);
     font-weight: 300;

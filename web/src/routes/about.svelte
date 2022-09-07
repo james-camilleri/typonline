9f2e955d4dd5a86a87b6916fd9504ddf0d7a812d
James Camilleri <james@james.mt>
2022-09-07 11:20:51 +0200

    Tweak spacing

--- web/src/routes/about.svelte
+++ web/src/routes/about.svelte
@@ -1,60 +1,62 @@
-<h2>the project</h2>
-<p>
-  Typo is an interactive performance between an audience and a vintage
-  typewriter that has a life of its own. Animated by hidden robotics and custom
-  AI software, it writes and creates poetry and text in a dialogue with its
-  human audience.
-</p>
-<p>
-  <iframe
-    src="https://www.youtube.com/embed/5LoYcGrAqkc"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-    style="width: 100%; aspect-ratio: 16/9; border-radius: var(--border-radius);"
-  />
-</p>
-<p>
-  The mechanical typewriter is an anachronism, relegated to a bygone era, and
-  yet is still a potent and recognised symbol of art and literature, of
-  mechanisation and progress. By splicing together the digital and the analogue,
-  the mechanical and the electronic, we breathe new life into the machine and
-  turn it into the primary actor on stage, capable of responding to its
-  surroundings and creating of its own accord. The machine becomes a focal point
-  for interaction, and gives us the opportunity to give experiences that may
-  previously have been purely digital (many forms of text-based interaction) a
-  new physical dimension.
-</p>
-<p>
-  In the spirit of creating moments of wonder and astonishment, Typo offers
-  audiences the chance to interact with an enigmatic machine, representative of
-  a long history of literature and industry. No longer a simple tool, it gains a
-  new role as an autonomous co-creator alongside its audience.
-</p>
+<div>
+  <h2>the project</h2>
+  <p>
+    Typo is an interactive performance between an audience and a vintage
+    typewriter that has a life of its own. Animated by hidden robotics and
+    custom AI software, it writes and creates poetry and text in a dialogue with
+    its human audience.
+  </p>
+  <p>
+    <iframe
+      src="https://www.youtube.com/embed/5LoYcGrAqkc"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      style="width: 100%; aspect-ratio: 16/9; border-radius: var(--border-radius);"
+    />
+  </p>
+  <p>
+    The mechanical typewriter is an anachronism, relegated to a bygone era, and
+    yet is still a potent and recognised symbol of art and literature, of
+    mechanisation and progress. By splicing together the digital and the
+    analogue, the mechanical and the electronic, we breathe new life into the
+    machine and turn it into the primary actor on stage, capable of responding
+    to its surroundings and creating of its own accord. The machine becomes a
+    focal point for interaction, and gives us the opportunity to give
+    experiences that may previously have been purely digital (many forms of
+    text-based interaction) a new physical dimension.
+  </p>
+  <p>
+    In the spirit of creating moments of wonder and astonishment, Typo offers
+    audiences the chance to interact with an enigmatic machine, representative
+    of a long history of literature and industry. No longer a simple tool, it
+    gains a new role as an autonomous co-creator alongside its audience.
+  </p>
 
-<h2>the artists</h2>
-<p>
-  <strong>James Camileri</strong> is a software engineer by day, but has
-  experience in graphic and motion design, and is wildly passionate about the
-  theatre, whether it's on stage as a performer or as part of a production team.
-  With a burgeoning interest in generative digital art, he is keen on creating
-  things that break out of the digital space and into the real world. (<a
-    href="mailto:james@james.mt">james@james.mt</a
-  >)
-</p>
-<p>
-  <strong>Evina Kipeni-Thalassinou</strong> is a visual and performance artist
-  with major in painting. Her main interest is to create installations where the
-  audience can experience wonder and magic. (<a
-    href="mailto:evinakat2@hotmail.com">evinakat2@hotmail.com</a
-  >)
-</p>
+  <h2>the artists</h2>
+  <p>
+    <strong>James Camileri</strong> is a software engineer by day, but has
+    experience in graphic and motion design, and is wildly passionate about the
+    theatre, whether it's on stage as a performer or as part of a production
+    team. With a burgeoning interest in generative digital art, he is keen on
+    creating things that break out of the digital space and into the real world.
+    (<a href="mailto:james@james.mt">james@james.mt</a>)
+  </p>
+  <p>
+    <strong>Evina Kipeni-Thalassinou</strong> is a visual and performance artist
+    with major in painting. Her main interest is to create installations where
+    the audience can experience wonder and magic. (<a
+      href="mailto:evinakat2@hotmail.com">evinakat2@hotmail.com</a
+    >)
+  </p>
 
-<h2>the technology</h2>
-<p>
-  Typo is open source! You can check out the code for the Raspberry-Pi-powered
-  mechanics <a href="https://github.com/james-camilleri/typewriter-dmx">here</a
-  >, and the code for the web platform and APIs
-  <a href="https://github.com/james-camilleri/typonline">here.</a>
-</p>
+  <h2>the technology</h2>
+  <p>
+    Typo is open source! You can check out the code for the Raspberry-Pi-powered
+    mechanics <a href="https://github.com/james-camilleri/typewriter-dmx"
+      >here</a
+    >, and the code for the web platform and APIs
+    <a href="https://github.com/james-camilleri/typonline">here.</a>
+  </p>
+</div>

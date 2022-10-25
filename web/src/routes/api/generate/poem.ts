9fcbfc945728bdb73c036e6cd2a48e3883ab59d9
James Camilleri <james@james.mt>
2022-10-25 16:31:22 +0100

    Update poem prompt

--- web/src/routes/api/generate/poem.ts
+++ web/src/routes/api/generate/poem.ts
@@ -1,4 +1,7 @@
+import shuffle from 'lodash/shuffle.js'
 import { Configuration, OpenAIApi } from 'openai'
+
+import poemSeeds from './_seeds'
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -11,16 +14,21 @@ export async function POST({ request }) {
   try {
     const payload = await request.json()
     const { seeds } = payload
-    const prompt = `a poem with the words ${seeds.join(', ')}`
+    const seedPoem = shuffle(poemSeeds.flat()).slice(0, 20).join('\n')
+    const prompt = `${seedPoem}\n\nwrite a poem using the words ${seeds.join(
+      ', ',
+    )} in the style of e e cummings:`
+    console.log(prompt)
 
     const response = await openai.createCompletion({
       prompt,
       model: 'text-davinci-002',
-      temperature: 0.8,
+      temperature: 0.6,
       max_tokens: 150,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
+      top_p: 0.8,
+      best_of: 3,
+      frequency_penalty: 1.7,
+      presence_penalty: 1.5,
     })
 
     const poem = response.data.choices[0].text

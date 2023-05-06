8a34f4d0f3d5fb4383c5f7e09b733c247cfe74dc
James Camilleri <james@james.mt>
2023-05-06 17:17:15 +0100

    Add creative coding talk link

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -22,7 +22,14 @@
       Typo has been exhibited at the Design for Performance and Interaction show
       at Bloomsbury Theatre in March 2022, the London Festival of Architecture
       2022, TEDx at Goodenough College, the Ars Electronica international arts
-      festival in Linz, and the PUSH UX Conference in Berlin.
+      festival in Linz, and the PUSH UX Conference in Berlin. A talk about the
+      project was presented at the London Creative Coding meetup in February
+      2023, a recording of which can be seen <a
+        target="_blank"
+        rel="noopener"
+        href="https://www.youtube.com/live/L2U_Sd1qMJ4?feature=share&t=1032"
+        >here</a
+      >.
     </p>
     {#if data.conversations.length > 0}
       <p>

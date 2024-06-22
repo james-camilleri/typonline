86720cacd3ee40f21d53daa6769790589579ec71
James Camilleri <james.camilleri@fx.sgx.com>
2024-06-22 16:15:53 +0100

    Add basic smoke tests

---
+++ sites/web/tests/regression.mobile.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+import { generateVisualTestParams, goToAndWait } from './utils'
+
+test('mobile visual regression', async ({ page }) => {
+  await page.goto('/')
+  const testParams = await generateVisualTestParams(page)
+
+  for (const { name, url } of testParams) {
+    await goToAndWait(page, url)
+
+    await test.step(`screenshot "${name}"`, async () => {
+      await expect(page).toHaveScreenshot(`${name}--.png`, {
+        fullPage: true,
+      })
+    })
+  }
+})
